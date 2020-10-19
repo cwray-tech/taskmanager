255040c657131cf96872c267014c33902fb00eed
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-19 15:30:34 -0600

    Ran production npm and updated task button component

--- resources/js/Components/TaskButtons.vue
+++ resources/js/Components/TaskButtons.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="md:flex flex-wrap items-center justify-between mb-6">
-        <div class="flex items-center space-x-2 flex-wrap">
-            <ButtonLink :href="route('tasks.index')" :active="$page.currentRouteName == 'tasks.index'">All Tasks</ButtonLink>
-            <ButtonLink :href="route('tasks.pending')" :active="$page.currentRouteName == 'tasks.pending'">Pending Tasks</ButtonLink>
-            <ButtonLink :href="route('tasks.completed')" :active="$page.currentRouteName == 'tasks.completed'">Completed Tasks</ButtonLink>
+        <div class="flex items-center flex-wrap">
+            <ButtonLink class="mr-2" :href="route('tasks.index')" :active="$page.currentRouteName == 'tasks.index'">
+                All
+            </ButtonLink>
+
+            <ButtonLink class="mr-2" :href="route('tasks.pending')" :active="$page.currentRouteName == 'tasks.pending'">
+                Pending
+            </ButtonLink>
+
+            <ButtonLink class="mr-2"  :href="route('tasks.completed')" :active="$page.currentRouteName == 'tasks.completed'">
+                Completed
+            </ButtonLink>
         </div>
-        <ButtonLink class="mt-2 md:mt-0" :href="route('tasks.create')" :active="$page.currentRouteName == 'tasks.create'">New Task</ButtonLink>
+        <ButtonLink class="mt-2 md:mt-0" :href="route('tasks.create')" :active="$page.currentRouteName == 'tasks.create'">
+            New Task
+        </ButtonLink>
     </div>
 </template>
 <script>
